a4ccaa8a3caa6154d7d48ad2462143a5a1811655
Jeremy Case <jeremycase@odysseyconsult.com>
2025-07-17 11:31:49 -1000

    dependency change

--- common/library/dao/src/main/java/org/ubiquia/common/library/dao/controller/GenericUbiquiaDaoController.java
+++ common/library/dao/src/main/java/org/ubiquia/common/library/dao/controller/GenericUbiquiaDaoController.java
@@ -1,4 +1,4 @@
-package org.ubiquia.core.flow.controller;
+package org.ubiquia.common.library.dao.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
